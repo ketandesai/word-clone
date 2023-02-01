2969932b42c6cf55cc43bbdd7958369becde9e12
Ketan Desai <desai.ketan@gmail.com>
2023-02-01 21:24:03 +0000

    excerise 3

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { range } from "../../utils";
 
-function Guess({guess}) {
+function Guess({value}) {
   return (
-    <>
+    <p className="guess">
       {range(5).map((column) => (
-        <span key={column} className="cell">{guess?.charAt(column)}</span>
+        <span key={column} className="cell">{value ? value[column]: undefined}</span>
       ))}
-    </>
+    </p>
   );
 }
 
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -7,9 +7,7 @@ function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((row) => (
-        <p key={row} className="guess">
-          <Guess guess={guesses[row]}/>
-        </p>
+          <Guess key={row} value={guesses[row]}/>
       ))}
     </div>
   );
